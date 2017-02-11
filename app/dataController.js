3262e468b605c265d3489ce66f293e57e98a0f23
podgito <padraic.duffy.1@gmail.com>
2017-02-11 13:35:40 +0000

    Init

---
+++ app/dataController.js
@@ -0,0 +1,23 @@
+(function() {
+'use strict';
+
+    angular
+        .module('app')
+        .controller('dataController', dataController);
+
+    dataController.inject = ['dataService'];
+    function dataController(dataService) {
+        var vm = this;
+        
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            var list = dataService.wordCloudData();
+            WordCloud(document.getElementById('wordCloud'), { list: list , weightFactor: 0.09 } );
+
+         }
+    }
+})();
